a8fae4cca5df10f7d9283efdd7dcd4dd58fcf680
Auriane Gillet <103597382+AurianeGillet@users.noreply.github.com>
2023-06-06 12:17:41 +0200

    CPP04

---
+++ ex02/main.cpp
@@ -0,0 +1,24 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main( void )
+{
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    delete j;
+    delete i;
+
+    Dog basic;
+    {
+        Dog tmp = basic;
+    }
+
+    const Animal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
+    for ( int i = 0; i < 4; i++ ) {
+        animals[i]->makeSound();
+        delete animals[i];
+    }
+
+    return 0;
+}
